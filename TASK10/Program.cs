f9b66326401c783728ec2ec08c1aae01624098c9
RTWalther <mr.tohtaev1998@mail.ru>
2022-10-24 02:09:57 +0500

    DZZ

---
+++ TASK10/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите программу, которая 
+//  1. принимает на вход трёхзначное число 
+//  2. и на выходе показывает вторую цифру этого числа.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int amount = num.ToString().Length;
+
+if (amount < 3 || amount > 3)
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
+int Secondigit(int number)
+{
+    int secondDigit = (number/10)%10;
+    return secondDigit; 
+}
+int finalNum = Secondigit(num);
+Console.WriteLine(finalNum);
---
+++ TASK13/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая 
+// 1.выводит третью цифру заданного числа
+// 2.или сообщает, что третьей цифры нет. 
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int ThirdDigit(int number)
+{
+ int firstDigit = (number / 100) * 100;
+    int thirdDigit = number % 10;
+    int secondDigit = ((number - firstDigit - thirdDigit) / 10);
+    //int thirdDigit = secondDigit % 10;
+    return secondDigit;
+}
+int thirdDigit = ThirdDigit(num);
+if (num <= 99)
+{
+    Console.WriteLine($"третьей цифры нет.");
+}
+else
+{
+    Console.WriteLine(ThirdDigit(num));
+}
+
